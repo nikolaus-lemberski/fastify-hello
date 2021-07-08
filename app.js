17aa8e1aef8966e77b965394352dd2f59994ac51
Nikolaus Lemberski <nlemberski@pivotal.io>
2021-07-08 12:03:26 +0200

    Start command

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({
-    logger: true
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
   })
-  
-  // Declare a route
-  fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
-  })
-  
-  // Run the server!
-  fastify.listen(3000, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-  })
+}
